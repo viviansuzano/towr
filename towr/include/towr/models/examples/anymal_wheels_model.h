7d900b5dbc12c11c68c56b7c05056d0987adc9a5
Vivian Suzano Medeiros <vivian.suzanomedeiros@mavt.ethz.ch>
2019-04-10 16:01:04 +0200

    The z-motion of the wheels are now constrained by the leg extension (WheelsMotionConstraint)

--- towr/include/towr/models/examples/anymal_wheels_model.h
+++ towr/include/towr/models/examples/anymal_wheels_model.h
@@ -30,7 +30,7 @@ public:
     nominal_stance_.at(LH) << -x_nominal_b,   y_nominal_b, z_nominal_b;
     nominal_stance_.at(RH) << -x_nominal_b,  -y_nominal_b, z_nominal_b;
 
-    max_dev_from_nominal_ << 0.15, 0.1, 0.10;
+    max_dev_from_nominal_ << 0.15, 0.1, 10.0;
 
     const double x_nominal_hip = 0.3405;
     const double y_nominal_hip = 0.1710;
